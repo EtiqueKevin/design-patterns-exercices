42a7c265b7ae6d0f0642ea91df29cf033f7d22f4
EtiqueKevin <ketique04@gmail.com>
2025-03-10 15:34:34 +0100

    fin observers

--- observer/app/MusicBand.php
+++ observer/app/MusicBand.php
@@ -2,15 +2,18 @@
 
 namespace App;
 
-class MusicBand 
+use SplObjectStorage;
+use SplObserver;
+use SplSubject;
+
+class MusicBand implements SplSubject
 {
+    private $observers;
     // Hors exercice mais notable:
     // Promotion du constructeur: https://www.php.net/manual/fr/language.oop5.decon.php#language.oop5.decon.constructor.promotion
-    public function __construct(
-        private string $name,
-        private array $concerts = []
-    ) {}
-
+    public function __construct(private string $name, private array $concerts = []) {
+        $this->observers = new SplObjectStorage();
+    }
 
     public function addNewConcertDate(string $date, string $location):void
     {
@@ -18,11 +21,24 @@ class MusicBand
             'date' =>  $date,
             'location' => $location
         ];
+
+        $this->notify();
     }
 
-    public function attach(): void 
-    {}
+    public function attach(SplObserver $observer): void
+    {
+        $this->observers->attach($observer);
+    }
 
-    public function detach(): void 
-    {}
+    public function detach(SplObserver $observer): void
+    {
+        $this->observers->detach($observer);
+    }
+
+    public function notify(): void
+    {
+        foreach ($this->observers as $observer) {
+            $observer->update($this);
+        }
+    }
 }
